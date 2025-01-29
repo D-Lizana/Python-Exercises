01dc634c698c247fc72e413f01aaf4a290e2c8a2
Diego Lizana <helgait@usal.es>
2025-01-29 20:02:37 +0100

    Easy for loop

---
+++ ejer4loops.py
@@ -0,0 +1,4 @@
+# OK (L1) 9. Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás 
+
+for i in range(360,159,-20):
+    print(i)
---
+++ ejer5loops.py
@@ -0,0 +1,15 @@
+# OK (L2) 11. Escribe un programa que calcule la media de un conjunto de números positivos introducidos por teclado.
+#  A priori, el programa no sabe cuántos números se introducirán. El usuario indicará que ha terminado de introducir los datos cuando meta un número negativo. 
+
+contador = 0
+sumador = 0
+numero = 0
+
+while numero >= 0:
+    
+    numero = int(input("Introduce un numero: "))
+    if numero >= 0:
+        sumador+=numero
+        contador+=1
+
+print(f"Media: {sumador/contador}")
