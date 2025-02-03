e2f0f86d235ffc1d0fca1c5ad9aae5b4724eb1e0
Diego Lizana <helgait@usal.es>
2025-02-03 15:07:42 +0100

    Started other arrays exercises

---
+++ ejer13array.py
@@ -0,0 +1,1 @@
+# (L2) 3. Escribe un programa que genere aleatoriamente un array de 15 componentes y muestre los valores máximo y mínimo junto con las palabras “máximo” y “mínimo” al lado respectivamente.
---
+++ ejer14arrays.py
@@ -0,0 +1,3 @@
+# (L3) 4. Escribe un programa que genere aleatoriamente un array de 20 componentes y rote los elementos del array, es decir,
+#  el elemento de la posición 0 debe pasar a la posición 1, el dela 1 a la 2, etc. El número que se encuentra en la última posición debe pasar a la posición 0.
+#  Finalmente, mostrar el contenido del array. 
