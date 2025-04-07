62dc031445cb1160c5fb90b059f88cd244554b70
Diego Lizana <helgait@usal.es>
2025-04-07 14:07:25 +0200

    Resuelto ejercicio para saber si un String es palindromo o no

---
+++ EntornosDesarrollo/RetoPalindromo.py
@@ -0,0 +1,12 @@
+frase = input("Introduce una frase: ")
+palindromo = True
+
+for i in range(len(frase)//2):
+    if frase[i] != frase[len(frase)-i-1]:
+        palindromo = False
+        break
+
+if palindromo == False:
+    print("La frase no es un palíndromo.")
+else:
+    print("La frase es un palíndromo.")
