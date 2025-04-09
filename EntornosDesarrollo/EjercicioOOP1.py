558d2c1a3ac57e45cf703f13e56cf74cecfd4899
Diego Lizana <helgait@usal.es>
2025-04-09 12:41:20 +0200

    Added 1:n relatiion and main

--- EntornosDesarrollo/EjercicioOOP1.py
+++ EntornosDesarrollo/EjercicioOOP1.py
@@ -5,7 +5,7 @@ class Empleado:
     def __init__(self, id_empleado, nombre, salario):
         self.id_empleado = id_empleado
         self.nombre = nombre
-        self-salario = salario
+        self.salario = salario
 
     # Metodo
     def calcular_bonificacion(self):
@@ -21,9 +21,41 @@ class Gerente(Empleado):
     def __init__(self, id_empleado, nombre, salario, departamento):
         self.id_empleado = id_empleado
         self.nombre = nombre
-        self-salario = salario
+        self.salario = salario
         self.departamento = departamento
 
     def aprobar_proyecto(self):
         print(f"Gerente {self.nombre} del departamento {self.departamento} aprueba el proyecto.")
 
+class Departamento:
+    def __init__(self, id_departamento, nombre):
+        self.id_departamento = id_departamento
+        self.nombre = nombre
+        self.empleados = []
+
+    def agregar_empleado(self, empleado):
+        self.empleados.append(empleado)
+
+    def eliminar_empleado(self, empleado):
+        if empleado in self.empleados:
+            self.empleados.remove(empleado)
+
+    def mostrar_empleados(self):
+        for empleado in self.empleados:
+            print(empleado.mostrar_informacion())
+
+
+def main():
+    empleado1 = Empleado(1, "Diego", 50000)
+    gerente1 = Gerente(2, "Javi", 60000, "Ciberseguridad")
+
+    departamento1 = Departamento(66, "Ciberseguridad")
+
+    departamento1.agregar_empleado(gerente1)
+    departamento1.agregar_empleado(empleado1)
+
+    departamento1.mostrar_empleados()
+
+if __name__ == "__main__":
+    main()
+    
