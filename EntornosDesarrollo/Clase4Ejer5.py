54c30af8b5424d09a8749ea0374789f8a33bc986
Diego Lizana <helgait@usal.es>
2025-04-24 11:34:21 +0200

    Easy while exercise

--- EntornosDesarrollo/Clase4Ejer5.py
+++ EntornosDesarrollo/Clase4Ejer5.py
@@ -6,3 +6,14 @@
 # 3.Cuando el usuario introduzca un número negativo, el programa finaliza y muestra todos
 # los números introducidos (puedes mostrarlos en una lista).
 
+numeros = []
+condicion = True
+
+while(condicion):
+    numero = int(input("Introduce un número entero: "))
+    if numero>=0:
+        numeros.append(numero)
+    else:
+        condicion = False
+
+print(numeros)
