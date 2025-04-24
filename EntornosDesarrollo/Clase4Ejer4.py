397951d50f5931bdd51b94b65941f85d732f3881
Diego Lizana <helgait@usal.es>
2025-04-24 11:22:22 +0200

    Easy for loop

--- EntornosDesarrollo/Clase4Ejer4.py
+++ EntornosDesarrollo/Clase4Ejer4.py
@@ -3,3 +3,11 @@
 # 1.Crea una lista de 5 números, por ejemplo: [5, 2, 7, 1, 9].
 # 2.Utilizando un bucle for, recorre la lista y calcula la suma total de los elementos.
 # 3.Imprime el resultado de la suma.
+
+numeros = [5,2,7,1,9]
+contador = 0
+
+for i in range(len(numeros)):
+    contador += numeros[i]
+
+print(f"{contador}")
