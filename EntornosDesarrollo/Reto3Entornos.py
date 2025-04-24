59b3f72563dd23a9216b9e09ba4fc3f58cb2c541
Diego Lizana <helgait@usal.es>
2025-04-24 21:15:14 +0200

    Reto 3 solved

---
+++ EntornosDesarrollo/Reto3Entornos.py
@@ -0,0 +1,33 @@
+# Se necesita un programa que haga una operación matemática (por ejemplo 37 + 12).  
+# Características: 
+#   • El programa se seguirá ejecutando hasta que yo de la respuesta 
+# correcta.   
+#   • Si fallo el programa me debe decir si me he pasado o me he quedado 
+# corto. 
+#   • Puedo introducir por pantalla los dos números que quiero que se 
+# sumen, por lo tanto, la suma ya no es siempre la misma, si no la que el 
+# usuario elija.   
+#   • Controlar los posibles errores.
+
+try:
+    numero1 = float(input("Introduce el primer número: "))
+    numero2 = float(input("Introduce el segundo número: "))
+
+    solucion = False
+
+    while(solucion==False):
+        resultado = float(input(f"Resuelve la suma {numero1}+{numero2}="))
+
+        if resultado == (numero1+numero2):
+            print("Resultado correcto.")
+            solucion = True
+        else:
+            print("Resultado incorrecto.")
+
+
+except ValueError:
+    print("Número no válido")
+
+finally:
+    print("Operacion terminada.")
+
