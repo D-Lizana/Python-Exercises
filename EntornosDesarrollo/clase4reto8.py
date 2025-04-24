6c6916df6c2726c964b7c890786ceb3f4ec0a477
Diego Lizana <helgait@usal.es>
2025-04-24 20:25:29 +0200

    Recursive functions lesson challenge

--- EntornosDesarrollo/clase4reto8.py
+++ EntornosDesarrollo/clase4reto8.py
@@ -3,3 +3,16 @@
 # 3 + 4). Demuestra el funcionamiento de la función solicitando un número al usuario y mostrando el
 # resultado de la suma de sus dígitos.
 
+def suma_digitos(numero):
+    if numero < 10:
+        return numero
+    else:
+        # con numero%10 aislamos el ultimo numero, y con numero//10 cogemos el resto y lo volvemos a pasar a suma_digitos
+        return numero%10 + suma_digitos(numero//10)
+
+
+def main():
+    numero = int(input("Introduce un número: "))
+    print(suma_digitos(numero))
+
+main()
