3bb044c902c84d88492e3b3ade24dbbc25a8c10d
Diego Lizana <helgait@usal.es>
2025-04-24 20:34:11 +0200

    Easy exceptions lesson challenge

--- EntornosDesarrollo/clase4reto9.py
+++ EntornosDesarrollo/clase4reto9.py
@@ -8,3 +8,18 @@
 # 3. En cualquier caso, usar un bloque finally para imprimir un mensaje indicando que la
 # operación ha finalizado.
 # 4. Si la selección es exitosa, mostrar el color correspondiente.
+
+colores = ["rojo","verde","azul","amarillo"]
+
+try:
+    numero = int(input("Introduce un número: "))
+    print(f"El color seleccionado es: {colores[numero]}")
+
+except ValueError:
+    print("Número no válido.")
+
+except IndexError:
+    print("La lista no tiene tantos valores.")
+
+finally:
+    print("La operación ha finalizado.")
