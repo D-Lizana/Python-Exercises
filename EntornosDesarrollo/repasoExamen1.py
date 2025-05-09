e33df0aa9d1c9f2a19e249baff47343df3535dec
DLizanaDev <Helgait@usal.es>
2025-05-09 10:50:02 +0200

    Solved exercise 1 for practise

---
+++ EntornosDesarrollo/repasoExamen1.py
@@ -0,0 +1,9 @@
+def ejercicio1():
+    ciudad = input("Introduce ciudad: ")
+    habitantes = int(input("Introduce numero de habitantes: "))
+    print(f"{ciudad} tiene aproximadamente {habitantes} habitantes.")
+
+    habitantes = float(habitantes /1000)
+    print(f"Esto equivale a {habitantes} mil habitantes")
+
+    
