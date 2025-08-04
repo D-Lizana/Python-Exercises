c15309590eb93a604f81ce5ca923a825df3c69df
DLizanaDev <Helgait@usal.es>
2025-08-04 12:32:39 +0200

    Ejercicio 2 resuelto

--- EntornosDesarrollo/repasoPython.py
+++ EntornosDesarrollo/repasoPython.py
@@ -3,5 +3,15 @@ def ejercicio1():
     numero = int(input("Introduce número de habitantes: "))
 
     print(f"La ciudad de {ciudad} tiene {numero} habitantes.")
-    print(f"Esto equivale a {numero/1000} habitantes.")
+    print(f"Esto equivale a {numero/1000} mil habitantes.")
 
+def ejercicio2():
+    numero1 = int(input("Introduce un numero: "))
+    numero2 = int(input("Introduce otro numero: "))
+
+    try: 
+        resultado = numero1 / numero2
+        print(resultado)
+        
+    except ZeroDivisionError:
+        print("No se puedee dividir entre 0")
