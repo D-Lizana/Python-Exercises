675589e52a408aa4dd96a1e59f40a356a7871dbb
DLizanaDev <Helgait@usal.es>
2025-08-04 12:27:25 +0200

    Ejercicio 1 resuelto

--- EntornosDesarrollo/repasoPython.py
+++ EntornosDesarrollo/repasoPython.py
@@ -1,2 +1,7 @@
 def ejercicio1():
-    
+    ciudad = input("Introduce ciudad: ")
+    numero = int(input("Introduce número de habitantes: "))
+
+    print(f"La ciudad de {ciudad} tiene {numero} habitantes.")
+    print(f"Esto equivale a {numero/1000} habitantes.")
+
